246bbdafb291ae822e8af57a5f95a6651f4d5322
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-09-27 13:04:07 +0530

    Update x.py

--- x.py
+++ x.py
@@ -1,10 +1,10 @@
 import time
 import gmpy2
 start=time.time()
-for x in  range(10**6):
+for x in  range(10**4):
     m=(2**1000)+x
     if gmpy2.is_fermat_prp(m,2)==-1:
        print(m)
 stop=time.time()-start
 print(stop)
-print(10**6/stop)
+print(10**4/stop)
