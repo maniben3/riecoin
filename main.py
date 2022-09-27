6c2406ab59e5c7fa52f887ab9f43f1a2800bb423
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-09-27 13:13:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import pyximport; pyximport.install()
+import pyximport; pyximport.install
+import time
 import c
+start=time.time()
 print(c.eratosthenes(10 ** 9))
+stop=time.time()-start
+print(stop)
